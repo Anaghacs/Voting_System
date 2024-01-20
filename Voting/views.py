a1652ffd6d1d983985341c9ebbffb96299ccbd93
AnaghaCS <anaghasasidharan38@gmail.com>
2024-01-20 12:03:08 +0530

    Create signup or registration page  create heml page and views pages function and validation

--- Voting/views.py
+++ Voting/views.py
@@ -1,24 +1,51 @@
 from django.shortcuts import render,redirect
 from .models import Candidate,Vote
 from django.contrib.auth.models import User
+from django.contrib import messages
 
 # Create your views here.
 def index(request):
     return render(request,'base.html')
 
+#The user registration or signup form for the user.
 def signup(request):
-    # if request.method=="POST":
-        # username=request.POST['username']
-        # first_name=request.POST['first_name']
-        # last_name=request.POST['last_name']
-        # email_id=request.POST['email']
-        # password1=request.POST['password1']
-        # password2=request.POST['password2']
-        # user=User(username=username,first_name=first_name,last_name=last_name,email_id=email_id,password1=password1,password2=password2)
-        # user.save()
-        # print(user.objects.all())
-        return render(request,'signup.html')
+    #The form submition field daata fetch
+    if request.method=="POST":
+        username=request.POST['username']
+        first_name=request.POST['first_name']
+        last_name=request.POST['last_name']
+        email=request.POST['email']
+        password=request.POST['password1']
+        confirm_password=request.POST['password2']
+
+        #if condition checking username already exist then display the error message.
+        if User.objects.filter(username=username).exists():
+            messages.info(request,"Uersname is already exist! Please try some other username ")
+            return redirect('signup')
         
+
+        #if condition checking email id already exist then display the error message.
+        elif User.objects.filter(email=email).exists():
+            messages.info(request,"Email address already exist! Please try some other email address.")
+            return redirect('signup')
+        
+        
+        #if condition checking username already exist then display the error message.
+        if password !=confirm_password:
+            messages.info(request,"Password and confirm password didn't match !")
+            return redirect('signup')
+
+        else:
+
+            #create the 'User' objects
+            my_user=User.objects.create_user(username,email,password)
+
+            my_user.first_name=first_name
+            my_user.last_name=last_name
+            my_user.save()
+            return redirect('login')
+    return render(request,'signup.html')
+
 
 #Create admin home page functions.
 def admin_home(request):
